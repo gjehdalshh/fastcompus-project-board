2eca24220d59977528380096cfdcddf35ac9d27a
gjehdals <gjehdalshh@naver.com>
2023-07-18 21:00:02 +0900

    #10 - 도메인 클래스 설계 (게시글, 댓글)
    도메인 정보를 설계
    기초적인 게시글과 댓글 표현이 가능하도록함
    DB와 연동을 위한 추가적인 표현은 추후에 업데이트될 수 있음

--- src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
+++ src/main/java/com/fastcampus/projectboard/FastCampusProjectBoardApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FastCampusProjectBoardApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(FastCampusProjectBoardApplication.class, args);
     }
